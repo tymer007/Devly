3e885b15ffdda9d0e082afa2f5e9df46ecac3cec
EnochScript <enochfolorunso2018@gmail.com>
2024-07-09 15:31:18 +0100

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import SignUp from './pages/forms/SignUp.jsx'
 import Community from './pages/Community.jsx'
 import AboutUs from './pages/AboutUs.jsx'
 import WebDevProfile from './pages/profiles/WebDevProfile.jsx'
+import Setup from './pages/forms/SetUpProfile.jsx'
 
 import Profile from './pages/Dashboard/Profile.jsx'
 
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
       <Route path='/Login' element={< Login/>}/>
       <Route path='/SignUp' element={< SignUp/>}/>
+      <Route path='/Setup' element={< Setup/>}/>
 
       <Route path='/WebDevProfile' element={< WebDevProfile/>}/>
 
